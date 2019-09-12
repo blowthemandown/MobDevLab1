ce1d3ee019e76446e12081d9b00d7b5a1e0f8529
blowthemandown <jamescosgriffpiatt@gmail.com>
2019-09-11 21:30:00 -0400

    I don't know why the git submenu disapeared, so I'm starting over on github

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,25 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new ButtonListener());
+
     }
 }
+
+
